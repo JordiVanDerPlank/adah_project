c67f41408b9e3e00410a122ec03a5b7f62070756
Jordi van der Plank <jordivdp1@gmail.com>
2020-08-27 14:21:39 +0200

    Create triggerwords.py

---
+++ triggerwords.py
@@ -0,0 +1,79 @@
+# words that will change the current anger mode
+angerControl = ["christian", "chill", "angry yes"]
+
+# words that will activate a response
+speechWordsToActivate = [
+    "hello",
+    "perfect",
+    "but did you",
+    "exactly so shut the",
+    "thank you",
+    "hey ada",
+    "could you tell me a bit about yourself",
+    "yourself",
+    "son of a",
+    "sunniva",
+    "sweet",
+    "holy",
+    "what the",
+    "well",
+    "creeper",
+    "what"
+]
+
+chatWordsToActivate = [
+    # Greetings
+    # "hello",
+    # "hey adah",
+    # "hi",
+    # "hey",
+    # "sup",
+    # "what's up",
+    # "heya",
+
+    # Explanation
+    "could you tell me a bit about yourself",
+    "how do you work",
+    "how does she work",
+
+    # Generic
+    "perfect",
+    "the sword was cool",
+    "who is the best streamer?",
+    "who is the best streamer",
+    "i hate myself",
+    "tell me a joke",
+    "guess who's back"
+
+    # Actual cursing
+    "son of a",
+    "sweet",
+    "holy",
+    "what the",
+    "well",
+    "creeper",
+    "ssssss",
+    "fuck you",
+
+    # Thank you
+    "thank you",
+    "thanks",
+    "thx",
+
+    # Bye
+    "bye",
+    "goodbye",
+    "seeya",
+    "cya",
+    "adios",
+    "peace",
+
+    # Love
+    "i love you",
+
+    # RIP
+    "rip",
+    "rest in peace"
+
+]
+
