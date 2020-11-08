37869440d9d908ddbf373199f7889646a3734f61
Jordi van der Plank <jordivdp1@gmail.com>
2020-11-08 21:26:57 +0100

    Update CurseForMe.py
    Add user quotes to say out loud

--- CurseForMe.py
+++ CurseForMe.py
@@ -238,61 +238,11 @@ def checkMessages():
                     birthdayUser = birthdayUser.replace("@", "")
                     cursewords.SpeakText("Happy birthday    to you.    Happy birthday     to you.     Happy birthday dear " + birthdayUser + ".     Happy birthday   to you.")
 
-                if ("adah quote" in message):
-                    number = message.replace("adah quote", "")
-                    try:
-                        number = number.replace(" ", "")
-                    except:
-                        number = number
-                    print(type(number))
-                    print("quotenumber = " + number)
-                    # getData.printData("grugsey", quoteNumber)
-
-                    import requests
-                    page = requests.get("http://adah.theshelfman.net/quote.html")
-                    webpage = str((page.text.encode('utf8')))
-                    # firstPart = str(webpage).strip().split('~')
-                    print(webpage)
-
-                    # Get all the relevant content from the webpage
-                    content = webpage.split("~")
-
-                    # Get all the quotes from the webpage
-                    userAndQuotes = content[1].split("-")
-                    print("userquotes " + str(userAndQuotes))
-                    # print("content: " + content[1])
-                    for userAndQuote in userAndQuotes:
-                        # print("userAndQuote: " + userAndQuote)
-                        if ("!" not in userAndQuote):
-                            continue
-                        user = userAndQuote.split("!")[0]
-                        print(user)
-                        quoteNumber = userAndQuote.split()[1].split(":")[0].replace("quote ", "")
-                        if user == "grugsey":
-                            print("a")
-                            # do the thing
-                            if quoteNumber == number:
-                                thisMessage = ""
-                                print("b")
-                                try:
-                                    print("c")
-                                    print("rest = " + str(userAndQuote.split("!")))
-                                    print("quote & message = " + str(userAndQuote.split("!")[1].split(":")))
-                                    # thisMessage = userAndQuote.split("!")[1].split(":")[1]
-                                    thisMessage = userAndQuote.split("!")[1].split(":")[1].replace("\\r\\n", "")
-                                    print(thisMessage)
-                                    cursewords.SpeakText(thisMessage)
-                                except:
-                                    print("d")
-                                    thisMessage = userAndQuote.split()[1].split(":")[1]
-                                    cursewords.SpeakText(thisMessage)
-                                    
-                                break
-
-
-
-                        else:
-                            continue
+                #say user's Streamlabs quotes!
+                if "quote" in message and "!quote" not in message:
+                    messageWithOrigin = message.partition("[")
+                    print(messageWithOrigin[0])
+                    cursewords.SpeakText(messageWithOrigin[0])
                 
                 
                 # if subscribed:
